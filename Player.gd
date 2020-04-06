1d1fae059853eccb22f116562bc169b610e5e6c4
protozeit <sjlooaud@gmail.com>
2020-04-07 00:14:02 +0100

    first commit

---
+++ Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody
+
+var speed = 200
+var gravity = -9.8
+var direction = Vector3()
+var velocity = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	direction = Vector3(0, 0, 0)
+	if Input.is_action_pressed("ui_down"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.z -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.z += 1
+	
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+
+
+	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
+		velocity.y = 3
+		
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
---
+++ README.md
@@ -0,0 +1,2 @@
+# roll-a-ball
+A small game project with open source engine godot
